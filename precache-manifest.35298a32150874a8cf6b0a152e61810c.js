bdaf11c9337eedce3d43cfec55df5cc342fd37d8
Tom Ierino <19969307@student.curtin.edu.au>
2020-07-14 10:36:58 +0800

    Updates

---
+++ precache-manifest.35298a32150874a8cf6b0a152e61810c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e52c39ff97b05fa32b887c53c5e37b2",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "dd8331d1e9b7e5206e5b",
+    "url": "/covid-tracker/static/js/2.250d7e44.chunk.js"
+  },
+  {
+    "revision": "9654934d371dd3754c004c948da6904d",
+    "url": "/covid-tracker/static/js/2.250d7e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96637e0a10d1da8333c6",
+    "url": "/covid-tracker/static/js/main.cef2ee90.chunk.js"
+  },
+  {
+    "revision": "3f14d71cdf780154be65",
+    "url": "/covid-tracker/static/js/runtime-main.111a02c4.js"
+  }
+]);
